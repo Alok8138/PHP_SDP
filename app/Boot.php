e73f3d27a072e0e944bd8240d148b3a30434523f
Alok8138 <126945574+Alok8138@users.noreply.github.com>
2026-04-05 19:21:03 +0530

    product_application

---
+++ app/Boot.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'app/models/Core/Request.php';
+
+class Boot
+{
+    public static function init()
+    {
+        $request = new Model_Request();
+
+        $controllerName = $request->get('c', 'product');
+
+        $controllerFile = 'app/controllers/' . ucfirst($controllerName) . '.php';
+
+        if (!file_exists($controllerFile)) {
+            die("Controller file not found: " . $controllerFile);
+        }
+
+        require_once $controllerFile;
+
+        
+        $controllerClass = 'Controller_' . ucfirst($controllerName);
+
+        if (!class_exists($controllerClass)) {
+            die("Controller class not found: " . $controllerClass);
+        }
+
+        $controller = new $controllerClass();
+        $controller->dispatch();
+    }
+}
