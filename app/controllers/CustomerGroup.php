a7cec631c6301df4bb32521c80d0c7e23665cf1e
Alok8138 <126945574+Alok8138@users.noreply.github.com>
2026-04-06 11:03:08 +0530

    customergroup

---
+++ app/controllers/CustomerGroup.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once 'app/controllers/Core/Base.php';
+require_once 'app/models/CustomerGroup.php';
+
+
+class Controller_CustomerGroup extends COntroller_Core_Base
+{
+    public function listAction() {
+        $model = new CustomerGroup_Model();
+        $data = $model->fetchAll();
+
+        $this->renderTemplate('CustomerGroup/list.phtml',['data'=>$data]);
+
+    }
+    public function editAction() {
+        $model = new CustomerGroup_Model();
+        $id = $this->getRequest()->get('id');
+
+        if($id){
+            $model->load($id);
+        }
+
+        $this->renderTemplate('CustomerGroup/edit.phtml',['data'=>$model]);
+
+    }
+    public function saveAction() {
+        $model = new CustomerGroup_Model();
+
+        foreach($_POST['customergroup'] as $key=> $value){
+            $model->$key = $value;
+        }
+        $model->save();
+
+        $this->redirect('list', 'customergroup');
+    }
+    public function deleteAction() {
+        $id = $this->getRequest()->get('id');
+        if($id){
+            $model = new CustomerGroup_Model();
+            $model->delete($id);
+        }
+
+        $this->redirect('list', 'customergroup');
+        
+    }
+    
+}
---
+++ app/models/CustomerGroup.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'app/models/Core/Table.php';
+class CustomerGroup_Model extends Model_Core_Table
+{
+    protected $_tablename = "customer_group";
+    protected $_primarykey = "customer_group_id";
+}
